118ce86f505d92394d256bc3c52c43e5aeac194c
KazmaArakaki <twi1ightmessenger1988@gmail.com>
2020-03-29 20:01:27 +0900

    バグ修正

---
+++ templates/element/css/footer/default.php
@@ -0,0 +1,27 @@
+<style>
+.footer {
+  background-color: #352e23;
+  color: #fff4e6;
+}
+
+.footer-copyright {
+  font-size: 16px;
+}
+
+.footer li {
+  font-size: 4vw;
+}
+
+@media (min-width: /* md */ 768px) {
+  .footer li {
+    font-size: 3.2vw;
+  }
+}
+
+@media (min-width: /* lg */ 992px) {
+  .footer li {
+    font-size: 24px;
+  }
+}
+</style>
+
