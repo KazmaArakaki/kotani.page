52b7306d420b5206cd6b551a0eebd289b1927cb9
KazmaArakaki <twi1ightmessenger1988@gmail.com>
2020-03-31 18:42:04 +0900

    院長挨拶ページに準備中の文言を追加

--- templates/Home/hello.php
+++ templates/Home/hello.php
@@ -1,1 +1,12 @@
+<h2 class="text-center">
+  <?= __('院長あいさつ') ?>
+</h2>
+
+<div class="container py-4">
+  <p class="text-center">
+    <strong>
+      <?= __('現在準備中です。') ?>
+    </strong>
+  </p>
+</div>
 
